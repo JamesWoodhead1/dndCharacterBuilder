3e030b57d9fc2d653df75d83b272b6f428e9d6d6
James Woodhead <james_woodhead95@hotmail.co.uk>
2021-06-13 12:10:08 +0100

    added functionality to name the character

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,4 +4,9 @@ describe(`constructor`, () => {
   it(`returns an object`, () => {
     expect(new Character(`Razum-Dar`)).toBeInstanceOf(Object);
   });
+
+  it('sets the name property', () => {
+    const character = new Character('Razum-Dar');
+    expect(character.name).toEqual('Razum-Dar');
+  });
 });
