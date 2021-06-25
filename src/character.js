58709c53a6dee48f48f5440f3563448341c47711
James Woodhead <james_woodhead95@hotmail.co.uk>
2021-06-25 09:38:54 +0100

    refactored levelUp function and ensured tests were still passing

--- src/character.js
+++ src/character.js
@@ -4,9 +4,10 @@ function Character(name, role, race) {
   this.race = race;
   this.level = 1;
   this.exp = 0;
-  this.levelUp = function() {
-    this.level += 1;
-  }
+}
+
+Character.prototype.levelUp = function() {
+  this.level += 1;
 }
 
 Character.prototype.fight = function() {
